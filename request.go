24e2be02108d03715efca111881dc9a1b4fe6126
vanleantking <van.le.k3c1@gmail.com>
2019-03-04 11:46:35 +0700

    request message chat skype

--- request.go
+++ request.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	// "bytes"
-	// "encoding/json"
 	"fmt"
-
-	// "io/ioutil"
 	"net/http"
 
 	"./skype"
 )
 
 func main() {
-	activity := &skype.Activity{}
-
-	activity.Type = "event"
-	activity.Text = "another test message"
-	activity.From = skype.ChannelAccount{ID: skype.DMP_BOT_SKYPE, Name: skype.DMP_BOT}
-	activity.ServiceURL = skype.WebchatURL
 	client := &http.Client{}
 
 	sskype := skype.SkypeService{Client: client,
@@ -30,12 +20,12 @@ func main() {
 		panic(er.Error())
 	}
 	conversation := skype.Conversation{
-		Bot:     skype.ChannelAccount{ID: activity.From.ID, Name: activity.From.Name},
+		Bot:     skype.ChannelAccount{ID: skype.DMP_BOT_SKYPE, Name: skype.DMP_BOT},
 		IsGroup: false,
 		Members: []skype.ChannelAccount{
 			skype.ChannelAccount{ID: skype.DMP_GROUP_ID, Name: skype.DMP_GROUP_NAME}},
 		TopicName: "Start conversation"}
 	fmt.Println(conversation)
-	sskype.BeginConversation(conversation, activity.Text)
+	sskype.BeginConversation(conversation, "custom text")
 
 }
